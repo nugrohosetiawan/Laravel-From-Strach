3194454d044d38fe1a84b87619a6eba776e91038
Nugroho Setiawan <nugpro@gmail.com>
2018-01-15 00:31:53 +0700

    update ke 3

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,9 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
+        @include('inc.navbar')
+        <div class="container"> 
         @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layout.app')
 @section('content')
+    <div class="jumbotron text-center">
                 <h1> {{$title}}</h1>
                 <p>Semoga beruntung </p>
+                <p><a class="btn btn-primary btn-lg" href="/login" role="button"> Login</a>
+                    <a class="btn btn-success btn-lg" href="/register" role="button"> Register</a></p>
+    </div>
 
 @endsection
 
